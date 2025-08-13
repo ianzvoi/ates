5a4e52c02f65c2898763f2b6019d59ea3f027cda
IangZvoi <24281024@bjtu.edu.cn>
2025-08-13 19:43:02 +0800

    fix task creation issue.

--- src/tskman/switcher.s
+++ src/tskman/switcher.s
@@ -59,3 +59,33 @@ _load_tsk:
 	lw s10,  96(a1)
 	lw s11, 104(a1)
     ret
+
+#create a task with a task stack
+    .global _create_tsk
+_create_tsk:
+    sw a0, (a2)
+    sw a1, 8(a2) #todo INCORRECT
+    ret
+
+
+    .global _start_tsk
+_start_tsk:
+    sw sp ,    (a0)
+    sw ra ,   8(a0)
+    sw s0 ,  16(a0)
+    sw s1 ,  24(a0)
+    sw s2 ,  32(a0)
+    sw s3 ,  40(a0)
+    sw s4 ,  48(a0)
+    sw s5 ,  56(a0)
+    sw s6 ,  64(a0)
+    sw s7 ,  72(a0)
+    sw s8 ,  80(a0)
+    sw s9 ,  88(a0)
+    sw s10,  96(a0)
+    sw s11, 104(a0)
+
+    lw sp, (a1)
+    la ra, task_exit_handler
+    lw t0, 8(a1)
+    jalr x0, t0, 0
--- src/tskman/tsk.rs
+++ src/tskman/tsk.rs
@@ -1,16 +1,21 @@
-use core::arch::global_asm;
-use crate::tskman;
+use core::arch::{asm, global_asm};
+use crate::{power, tskman};
 use crate::tskman::tsk;
+use crate::uart::Uart;
 
 global_asm!(include_str!("switcher.s"));
 
 #[repr(C)]
 pub struct Tsk {
     sp : u64,
-    ra : u64,
+    pc : u64,
     reg : [u64; 12]
 }
-
+impl Tsk {
+    pub const fn new() -> Tsk {
+        Tsk{sp:0, pc:0, reg: [0; 12]}
+    }
+}
 extern "C" {
     ///  store context only.
     pub fn _sw_store(store : * mut Tsk);
@@ -18,4 +23,35 @@ extern "C" {
     /// this function (kinda of) is CALLED at appropriate position,
     /// only register s0 - s11 is needed to be saved.
     pub fn _switch_forced(store : * mut Tsk, load : * const Tsk );
+    pub fn _start_tsk(store : * mut Tsk, new : * const Tsk);
+
+    // TODO entrance should be saved elsewhere in TCB instead of ra.
+    pub fn _create_tsk(stack : *mut u8, entrance : *mut u8, tsk : *mut Tsk);
+
+
+}
+
+#[no_mangle]
+pub fn task_exit_handler() -> ! {
+    Uart::get().write(b"You can't return a tsk. PANIC.\n",31);
+    power::failure();
+}
+
+
+pub fn create_task(entrance : fn()) -> Tsk {
+    let mut p : Tsk = Tsk{
+        sp:0,
+        pc:0,
+        reg : [0; 12]
+    };
+    let mut stack: *mut u8;
+
+    unsafe {
+        asm!(
+            "la {v}, _task1_debug_stack_top",
+            v = out(reg) stack
+        );
+        _create_tsk(stack, entrance as *mut u8, &mut p);
+    }
+    p
 }
