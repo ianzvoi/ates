32d05c0c772c3d447d127a9ebd49c6b1394d907a
IangZvoi <24281024@bjtu.edu.cn>
2025-08-27 00:52:57 +0800

    refactor, prevent namespace pollution

--- src/tasks/locks/naive.rs
+++ src/tasks/locks/naive.rs
@@ -1,35 +1,86 @@
+//! # The Naive Lock (spin lock)
+//! 
+//! Usage:
+//! ```
+//!  naive::naive!(MY_LOCK);
+//! 
+//!  fn sensitive_function(){
+//!     naive::naive_lock!(MY_LOCK);
+//! 
+//!     /* access memory protected by the lock */
+//! 
+//!     naive::naive_unlock!(MY_LOCK);
+//! }
+//! ```
+//! 
+//! Basicaly The Naive Lock Works Likes this:
+//! ```rust
+//! fn lock(lc : mut ref):
+//!     while(lc is LOCKED):
+//!         W.F.I
+//!     lc <- LOCKED
+//!     
+//!
+//! fn unlock(lc : mut ref):
+//!     lc <- LOCKED
+//!
+//! ``` 
+//! but in atomic form.
+//!
+
 pub type NaiveLock = u8;
 
-#[macro_export] macro_rules! naive_lock {
-    ($lock:tt) => {
-        unsafe {
-            use core::arch::asm;
-            asm!(
-                "1:",
-                "mv t0, {lk}",
-                "li t1, 1",
-                "amoswap.w t1, t1, (t0)",
-                "beqz t1, 2f",
-                "wfi",
-                "J 1b",
-                "2:",
-                lk = in(reg) &raw mut $lock,
-            )
-        }
-    };
-}
 
-
-#[macro_export] macro_rules! naive_unlock {
-    ($lock:tt) => {
-        unsafe {
-            $lock = 0;
-        }
-    };
-}
-
-#[macro_export] macro_rules! naive {
-    ($name:ident) => {
-        static mut $name: NaiveLock = 0;
+/// ## try to get the lock
+/// If `lock` is LOCKED, it will fail in a loop
+/// that check if the lock is UNLOCKED,
+/// - if so, break the loop
+/// - if not, **it will freeze CPU** untill next interruption.  
+pub macro naive_lock ($lock:tt) {
+    unsafe {
+        use core::arch::asm;
+        asm!(
+            "1:",
+            "mv t0, {lk}",
+            "li t1, 1",
+            "amoswap.w t1, t1, (t0)",
+            "beqz t1, 2f",
+            "wfi",
+            "J 1b",
+            "2:",
+            lk = in(reg) &raw mut $lock,
+        )
     }
+}
+
+/// same as `naive_lock`
+/// 
+/// but this one won't freaze cpu.
+pub macro naive_lock_freaze_not ($lock:tt) {
+    unsafe {
+        use core::arch::asm;
+        asm!(
+            "1:",
+            "mv t0, {lk}",
+            "li t1, 1",
+            "amoswap.w t1, t1, (t0)",
+            "beqz t1, 2f",
+            "J 1b",
+            "2:",
+            lk = in(reg) &raw mut $lock,
+        )
+    }
+}
+
+/// ## unlock
+pub macro naive_unlock ($lock:tt) {
+    unsafe {
+        $lock = 0;
+    }
+}
+
+//TODO: make the lock dynamicaly acllocatble 
+/// ## create lock globaly
+pub macro naive ($name:ident) {
+    static mut $name: NaiveLock = 0;
 }
