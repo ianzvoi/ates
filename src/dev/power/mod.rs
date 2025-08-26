b8c5928e0ae5ef117d2e15bcb8a1406624057ee9
IangZvoi <24281024@bjtu.edu.cn>
2025-08-26 23:46:45 +0800

    update README

--- src/dev/power/mod.rs
+++ src/dev/power/mod.rs
@@ -1,10 +1,10 @@
 //! # Power management utilities
 //! In Qemu, no complicated power management is needed.
 //! send 3 magic word to terminate qemu process.
-//! 
+//!
 //!  qemu will intercept the write instruction to address 0x100000 and check if it is
 //!  one of these magic words:
-//! 
+//!
 //!     reset     - 0x7777
 //!     power off - 0x5555
 //!     fial      - 0x3333  (qemu should quit with none-zero return value.)
